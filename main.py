f0ae56ed594117c118c3419d7bcdf4210b17d1da
Konrad Gawron <konrad.gawron@poczta.fm>
2021-01-12 18:00:23 +0100

    Add almost final

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
+    choosen_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=choosen_list)
     random.shuffle(movies)
-    return render_template("homepage.html", movies=movies)
+    return render_template("homepage.html", movies=movies, choosen_list=choosen_list)
 
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast, images=movie_images)
 
 @app.context_processor
 def utility_processor():
--- tmdb_client.py
+++ tmdb_client.py
@@ -8,12 +8,35 @@ def get_popular_movies():
     response = requests.get(url, headers=headers)
     return response.json()
 
+def get_movies_list(list_type):
+    url = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
 def get_poster_url(poster_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_path}"
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
     return data["results"][:how_many]
 
+def get_single_movie(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    return response.json()
 
+def get_single_movie_cast(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_movie_images(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    return response.json()
